ab355025b83b3aaced60481b7f8ae7605fa6e383
Matthieu Bergel <hello@matthieubergel.org>
2019-04-03 18:50:01 +0200

    feat: add code item animation (w/ react hooks and gsap)

--- src/components/Code/Item/Item.jsx
+++ src/components/Code/Item/Item.jsx
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
+import { TweenLite } from 'gsap';
 import styles from './Item.module.scss';
 
-const Item = ({ children, title, stack }) => (
-  <div className={styles.item}>
-    <h3>{title}</h3>
-    {children}
-    <div className={styles.stack}>{stack}</div>
-  </div>
-);
+const Item = ({
+  children, title, stack, transitionStatus,
+}) => {
+  const contentRef = useRef(null);
+  const [tween, setTween] = useState(null);
+
+  useEffect(() => {
+    // Will only trigger after the first render
+    if (tween === null) {
+      // https://codepen.io/anon/pen/bNxYqq
+      setTween(TweenLite.from(contentRef.current, 0.3, { height: 0, autoAlpha: 0 }).reversed(true));
+    }
+
+    if (transitionStatus === 'entering') {
+      tween.play();
+    } else if (transitionStatus === 'exiting') {
+      tween.reverse();
+    }
+  }, [transitionStatus]);
+
+  return (
+    <div className={styles.item}>
+      <h3>{title}</h3>
+      <div ref={contentRef} className={styles.content}>{children}</div>
+      <div className={styles.stack}>{stack}</div>
+    </div>
+  );
+};
+
+Item.defaultProps = {
+  transitionStatus: null,
+};
 
 Item.propTypes = {
   children: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   stack: PropTypes.string.isRequired,
+  transitionStatus: PropTypes.string,
 };
 
 export default Item;
--- src/hoc/WithLink.jsx
+++ src/hoc/WithLink.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import Transition from 'react-transition-group/Transition';
 import styles from './WithLink.module.scss';
 
 const WithLink = ({ children, href, ext }) => {
+  const [hover, setHover] = useState(null);
+
   const attr = { href };
   if (ext === true) {
     attr.target = '_blank';
     attr.rel = 'noreferrer noopener';
   }
 
+  const WrappedComponent = transitionStatus => (
+    React.Children.map(children, child => React.cloneElement(child, { transitionStatus })));
+
   return (
-    <a {...attr} className={styles.link}>
-      {children}
+    <a
+      {...attr}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+      className={styles.link}
+    >
+      <Transition in={hover} timeout={0}>
+        {transitionStatus => (
+          WrappedComponent(transitionStatus)
+        )}
+      </Transition>
     </a>
   );
 };
